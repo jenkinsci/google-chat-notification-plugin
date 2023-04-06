dc91f2c6bdceac72f697f276d5fb5fb7027666ec
Thiago Colbert <Ryudo302@users.noreply.github.com>
2023-04-06 01:29:55 -0300

    feat(Java): Update Java version to 11 (#36)
    * :package: feat(Java): Update Java version to 11
    - Use of HttpClient from Java instead of Apache
    
    ---------
    
    Co-authored-by: Jean-Baptiste Regazzoni <jean-baptiste.regazzoni@synergee.com>

--- src/main/java/io/cnaik/service/CommonUtil.java
+++ src/main/java/io/cnaik/service/CommonUtil.java
@@ -1,30 +1,16 @@
 package io.cnaik.service;
 
-import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
-import org.apache.commons.httpclient.util.URIUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpStatus;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.routing.HttpRoutePlanner;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
-import org.apache.http.util.EntityUtils;
 import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
 import org.jenkinsci.plugins.tokenmacro.TokenMacro;
+import org.springframework.web.util.UriUtils;
 
 import hudson.FilePath;
 import hudson.ProxyConfiguration;
@@ -43,7 +29,7 @@ public class CommonUtil {
     private LogUtil logUtil;
     private ResponseMessageUtil responseMessageUtil;
 
-    private static final int TIME_OUT = 15 * 1000;
+    private static final long TIME_OUT = 15 * 1000;
 
     public CommonUtil(GoogleChatNotification googleChatNotification) {
         this.googleChatNotification = googleChatNotification;
@@ -66,7 +52,7 @@ public class CommonUtil {
             return;
         }
 
-        String json = "";
+        String json;
 
         if (googleChatNotification.isCardMessageFormat()) {
             json = responseMessageUtil.createCardMessage();
@@ -99,9 +85,8 @@ public class CommonUtil {
                 CredentialUtil credentialUtil = new CredentialUtil();
                 StringCredentials stringCredentials = credentialUtil.lookupCredentials(url[1]);
 
-                if (stringCredentials != null
-                        && stringCredentials.getSecret() != null) {
-
+                if (stringCredentials != null) {
+                    stringCredentials.getSecret();
                     response = call(stringCredentials.getSecret().getPlainText(), json);
                 }
             }
@@ -120,7 +105,7 @@ public class CommonUtil {
             return result;
         }
 
-        Run prevRun = build.getPreviousBuild();
+        var prevRun = build.getPreviousBuild();
         Result previousResult = (prevRun != null) ? prevRun.getResult() : Result.SUCCESS;
 
         if (googleChatNotification.isNotifyAborted()
@@ -195,23 +180,22 @@ public class CommonUtil {
                     }
                 }
 
-                HttpPost post = new HttpPost(urlDetail);
-                StringEntity stringEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
-                post.setEntity(stringEntity);
-                post.setHeader("Content-type", ContentType.APPLICATION_JSON.toString());
+                var client = HttpClient.newHttpClient();
 
-                post.setConfig(getTimeoutConfig());
+                var request = HttpRequest.newBuilder()
+                        .uri(URI.create(urlDetail))
+                        .POST(HttpRequest.BodyPublishers.ofString(json))
+                        .timeout(Duration.ofSeconds(TIME_OUT))
+                        .build();
 
-                CloseableHttpClient client = getHttpClient();
-                CloseableHttpResponse response = client.execute(post);
+                var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-                int responseCode = response.getStatusLine().getStatusCode();
-                if (responseCode != HttpStatus.SC_OK) {
-                    HttpEntity entity = response.getEntity();
-                    String responseString = EntityUtils.toString(entity);
+                if (response.statusCode() != 200) {
+                    var body = response.body();
 
                     if (logUtil.printLogEnabled()) {
-                        logUtil.printLog("Google Chat post may have failed. Response: " + responseString + " , Response Code: " + responseCode);
+                        logUtil.printLog("Google Chat post may have failed. Response: " + body
+                                + " , Response Code: " + response.statusCode());
                     }
                 }
 
@@ -222,50 +206,8 @@ public class CommonUtil {
         }
         return false;
     }
-
+    
     private String getJobName() throws MacroEvaluationException, IOException, InterruptedException {
         return TokenMacro.expandAll(build, ws, taskListener, "${JOB_NAME}", false, null);
-    }
-
-    private CloseableHttpClient getHttpClient() {
-
-        final HttpClientBuilder clientBuilder = HttpClients.custom();
-        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        clientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-
-        if (Jenkins.getInstance() != null) {
-            ProxyConfiguration proxy = Jenkins.getInstance().proxy;
-            if (proxy != null) {
-                final HttpHost proxyHost = new HttpHost(proxy.name, proxy.port);
-                final HttpRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxyHost);
-                clientBuilder.setRoutePlanner(routePlanner);
-
-                String username = proxy.getUserName();
-                String password = proxy.getPassword();
-                // Consider it to be passed if username specified. Sufficient?
-
-                if (logUtil.printLogEnabled()) {
-                    logUtil.printLog("Using proxy authentication (user=" + username + "), (host=" + proxy.name + "), (port=" + proxy.port + ")");
-                }
-
-                if (username != null && !"".equals(username.trim())) {
-                    credentialsProvider.setCredentials(new AuthScope(proxyHost),
-                            new UsernamePasswordCredentials(username, password));
-                }
-            }
-        }
-
-        return clientBuilder.build();
-    }
-
-    private RequestConfig getTimeoutConfig() {
-
-        RequestConfig.Builder requestConfig = RequestConfig.custom();
-
-        requestConfig.setConnectTimeout(TIME_OUT);
-        requestConfig.setConnectionRequestTimeout(TIME_OUT);
-        requestConfig.setSocketTimeout(TIME_OUT);
-
-        return requestConfig.build();
     }
 }

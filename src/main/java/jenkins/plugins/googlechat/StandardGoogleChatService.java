a4bb6a25483c8258a2cc764a8d1f8d8dfed45843
Robin Müller <robin.mueller@ionos.com>
2024-03-25 10:49:03 +0100

    Send notifications via proxy if globally configured

--- src/main/java/jenkins/plugins/googlechat/StandardGoogleChatService.java
+++ src/main/java/jenkins/plugins/googlechat/StandardGoogleChatService.java
@@ -1,18 +1,26 @@
 package jenkins.plugins.googlechat;
 
+import hudson.ProxyConfiguration;
+import io.cnaik.Messages;
+import io.cnaik.model.google.MessageReplyOption;
+import jenkins.model.Jenkins;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.commons.lang3.StringUtils;
-
-import io.cnaik.Messages;
-import io.cnaik.model.google.MessageReplyOption;
 
 public class StandardGoogleChatService implements GoogleChatService {
 
@@ -99,7 +107,47 @@ public class StandardGoogleChatService implements GoogleChatService {
     }
 
     protected HttpClient getHttpClient() {
-        return HttpClient.newHttpClient();
+        HttpClient.Builder builder = HttpClient.newBuilder();
+        Jenkins jenkins = Jenkins.getInstanceOrNull();
+        if (jenkins != null) {
+            ProxyConfiguration proxy = jenkins.proxy;
+            if (proxy != null) {
+                builder.proxy(getProxySelector(proxy));
+                if (proxy.getUserName() != null) {
+                    builder.authenticator(getProxyAuthenticator(proxy));
+                }
+            }
+        }
+        return builder.build();
+    }
+
+    private static ProxySelector getProxySelector(ProxyConfiguration proxy) {
+        return new ProxySelector() {
+            @Override
+            public List<Proxy> select(URI uri) {
+                if (proxy.getNoProxyHostPatterns().stream().anyMatch(pattern -> pattern.matcher(uri.getHost()).matches())) {
+                    return List.of(Proxy.NO_PROXY);
+                }
+                return List.of(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxy.name, proxy.port)));
+            }
+
+            @Override
+            public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+
+            }
+        };
+    }
+
+    private static Authenticator getProxyAuthenticator(ProxyConfiguration proxy) {
+        return new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if (getRequestorType() == RequestorType.PROXY && getRequestingHost().equalsIgnoreCase(proxy.name) && getRequestingPort() == proxy.port) {
+                    return new PasswordAuthentication(proxy.getUserName(), proxy.getSecretPassword().getPlainText().toCharArray());
+                }
+                return null;
+            }
+        };
     }
 
     private boolean checkIfValidURL(String url) {
